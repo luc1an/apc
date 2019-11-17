2daf93632274946855ca957c6f1b48e64c88e713
luc1an <lucian.chiriac@gmail.com>
2019-11-17 10:10:25 +0200

    forgot to add some "using"s

--- apc.businesslayer.core/Helper.cs
+++ apc.businesslayer.core/Helper.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
 using HtmlAgilityPack;
 
 namespace apc.bussinesslayer.core
